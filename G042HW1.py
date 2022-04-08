42301f1531eec4b8477c4beb3d60762996f88332
Solmaz Mohammadi <solmaz454@gmail.com>
2022-04-08 19:44:24 +0200

    Update G042HW1.py

--- G042HW1.py
+++ G042HW1.py
@@ -27,17 +27,17 @@ def format_partition(dataset, S):
 
         # we filter based product and id based on the following proposition
         if quantity > 0 and (country == S or S == "all"):
-
             # by using the set add function we only get distinct value from this partition
             product_costumer.add((product, costumer))
 
     return [((product, costumer), 0) for (product, costumer) in product_costumer]
 
+
 def format_and_filter_dataset(dataset, S):
     filtered_dataset = (dataset
-                        .mapPartitions(lambda x: format_partition(x, S))    # <-- MAP PHASE (R1)
-                        .groupByKey()                                       # <-- SHUFFLE + GROUPING
-                        .keys())                                            # <-- REDUCE PHASE (R1)
+                        .mapPartitions(lambda x: format_partition(x, S))  # <-- MAP PHASE (R1)
+                        .groupByKey()  # <-- SHUFFLE + GROUPING
+                        .keys())  # <-- REDUCE PHASE (R1)
     return filtered_dataset
 
 
@@ -52,12 +52,13 @@ def partial_count(dataset):
             product_count[product] += 1
     return [(product, product_count[product]) for product in product_count.keys()]
 
+
 # Implementation of product popularity using MapPartition and MapValues
 def compute_popularity_1(product_costumer, K=1):
     product_popularity1 = (product_costumer
-                           .repartition(numPartitions=K)                            # <-- MAP PHASE (R1)
-                           .mapPartitions(partial_count)                              # <-- GROUPING + REDUCE PHASE (R1)
-                           .groupByKey()                                            # <-- SHUFFLE + GROUPING
+                           .repartition(numPartitions=K)  # <-- MAP PHASE (R1)
+                           .mapPartitions(partial_count)  # <-- GROUPING + REDUCE PHASE (R1)
+                           .groupByKey()  # <-- SHUFFLE + GROUPING
                            .mapValues(lambda partial_counts: sum(partial_counts)))  # <-- REDUCE PHASE (R2)
     return product_popularity1
 
@@ -66,14 +67,15 @@ def compute_popularity_1(product_costumer, K=1):
 # Implementation of product popularity using Map and ReduceByKey
 def compute_popularity_2(product_costumer, K=1):
     product_popularity2 = (product_costumer
-                           .map(lambda x: (x[0], 1))                            # <-- MAP PHASE (R1)
-                           .reduceByKey(lambda x, y: x + y, numPartitions=K))   # <-- REDUCE PHASE (R1) + (R2),
+                           .map(lambda x: (x[0], 1))  # <-- MAP PHASE (R1)
+                           .reduceByKey(lambda x, y: x + y, numPartitions=K))  # <-- REDUCE PHASE (R1) + (R2),
     # R1 works on partition and compute partial sums, R2 works on results of partition and compute full sums
     return product_popularity2
 
+
 # Part 5
 # top_H_reduce takes a partition of product popularity and a value H and return the H key value pair in
-# the partition with highest popularity
+# the partition with the highest popularity
 def top_H_reduce(partition, H):
     list_of_element = []
 
@@ -99,15 +101,15 @@ def top_H_reduce(partition, H):
 
 
 # We extract the top value in two round
-# in the first round we partition the data and we extract top H value from each partition
+# in the first round we partition the data, and we extract top H value from each partition
 # if H is bigger than the size of the partition we return all values.
-# We then have the RDD with all the TOP H values of each partition and we get
+# We then have the RDD with all the TOP H values of each partition, and we get
 # the top H value out of this
 def topH(product_popularity, H, K=1):
     partitioned_top_H = (product_popularity
                          .repartition(numPartitions=K)
-                         .mapPartitions(lambda x: top_H_reduce(x, H))       # <-- MAP PHASE
-                         .top(H, key=lambda x: x[1]))                       # <-- REDUCE PHASE
+                         .mapPartitions(lambda x: top_H_reduce(x, H))  # <-- MAP PHASE
+                         .top(H, key=lambda x: x[1]))  # <-- REDUCE PHASE
     return partitioned_top_H
 
 
@@ -148,11 +150,11 @@ def main():
     # 1) loading and partitioning the dataset
     rawData = sc.textFile(data_path, minPartitions=K).cache()
     rawData.repartition(numPartitions=K)
-    print("number of transactions = ", rawData.count())
+    print("Number of rows = ", rawData.count())
 
     # 2) filtering and converting the rawData to Product-Costumer pairs
     product_costumer = format_and_filter_dataset(rawData, S)
-    print("number of product costumer = ", product_costumer.count())
+    print("Product-Customer Pairs = ", product_costumer.count())
 
     # 3) computing product popularity using mapByPartition
     product_popularity_1 = compute_popularity_1(product_costumer, K)
@@ -163,12 +165,22 @@ def main():
     # 5) extracting the top H most popular items
     if H > 0:
         topHValues = topH(product_popularity_1, H, K)
-        print("Top =", topHValues)
+        print("Top ", H, " Products and their Popularities:")
+        for i in topHValues:
+            print("Product: ", i[0], " Popularity: ", i[1], end="; ")
 
-    # 6) printing the product popularity dataset
+    # 6) printing the sorted pairs of product-popularity
     if H == 0:
-        print("product popularity1 =", sorted(product_popularity_1.collect()))
-        print("product popularity2 =", sorted(product_popularity_2.collect()))
+        productPopularity1 = sorted(product_popularity_1.collect())
+        productPopularity2 = sorted(product_popularity_2.collect())
+
+        print("productPopularity1:")
+        for i in productPopularity1:
+            print("Product: ", i[0], " Popularity: ", i[1], end="; ")
+
+        print("productPopularity2:")
+        for j in productPopularity1:
+            print("Product: ", j[0], " Popularity: ", j[1], end="; ")
 
 
 if __name__ == "__main__":
